8c40733a329348a0e9104a2ae266f0dcaf6532bf
n.osipov <n.osipov@kadam.net>
2024-11-23 03:22:13 +0300

    fix: main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -113,9 +113,9 @@ resource "yandex_vpc_security_group" "security_group" {
 
 # Storage ресурсы
 resource "yandex_storage_bucket" "data_bucket" {
-  bucket     = "${var.yc_bucket_name}-${var.yc_folder_id}"
-  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket        = "${var.yc_bucket_name}-${var.yc_folder_id}"
+  access_key    = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key    = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
   force_destroy = true
 }
 
